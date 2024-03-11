93a8bdfa5723209309ce70c893a1096fbfe253b1
qqAndrey <63346175+botmadness111@users.noreply.github.com>
2024-03-11 14:13:12 +0300

    Initial commit

---
+++ src/main/java/ru/andrey/VkEducation/VkEducationApplication.java
@@ -0,0 +1,13 @@
+package ru.andrey.VkEducation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VkEducationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VkEducationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/andrey/VkEducation/VkEducationApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.andrey.VkEducation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VkEducationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
